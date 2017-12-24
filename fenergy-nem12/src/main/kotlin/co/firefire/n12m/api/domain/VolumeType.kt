509a14141f2cb39bcabf380114f761c67464fdf9
Tommy Li <tommy.li@firefire.co>
2017-12-24 15:58:57 +1100

    Remove unused

--- fenergy-nem12/src/main/kotlin/co/firefire/n12m/api/domain/VolumeType.kt
+++
@@ -1,8 +0,0 @@
-// Tommy Li (tommy.li@firefire.co), 2017-06-24
-package co.firefire.n12m.api.domain
-
-enum class VolumeType {
-    CONSUMPTION,
-    GENERATION,
-    OTHER
-}
--- fenergy-nem12/src/main/kotlin/co/firefire/n12m/api/repository/IntervalDayRepository.kt
+++
@@ -1,9 +0,0 @@
-// Tommy Li (tommy.li@firefire.co), 2017-06-24
-
-package co.firefire.n12m.api.repository
-
-import co.firefire.n12m.api.domain.IntervalDay
-import org.springframework.data.repository.PagingAndSortingRepository
-
-interface IntervalDayRepository : PagingAndSortingRepository<IntervalDay, Long> {
-}
